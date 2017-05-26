48c78fa1d1c91f65ea4813494893965cf511c541
심응준 <sim-eungjun@sim-eungjun-ui-Mac-Pro.local>
2017-05-26 21:41:24 +0900

    update

---
+++ Manager/E_Interactor.py
@@ -0,0 +1,163 @@
+import vtk
+import numpy as np
+
+class E_InteractorStyle(vtk.vtkInteractorStyle):
+    def __init__(self, Manager):
+        self.Mgr = Manager;
+        self.m_bProopagateEvent = True
+
+        self.m_bKeyDown = False
+
+        self.m_TransFactor = 0.1
+
+        self.AddObserver("MouseMoveEvent", self.MouseMoveEvent)
+        self.AddObserver("RightButtonPressEvent", self.RightButtonPressEvent)
+        self.AddObserver("RightButtonReleaseEvent", self.RightButtonReleaseEvent)
+        self.AddObserver("LeftButtonPressEvent", self.LeftButtonPressEvent)
+        self.AddObserver("LeftButtonReleaseEvent", self.LeftButtonReleaseEvent)
+        self.AddObserver("MiddleButtonPressEvent", self.MiddleButtonPressEvent)
+        self.AddObserver("MiddleButtonReleaseEvent", self.MiddleButtonReleaseEvent)
+        self.AddObserver("MouseWheelForwardEvent", self.MouseWheelForwardEvent)
+        self.AddObserver("MouseWheelBackwardEvent", self.MouseWheelBackwardEvent)
+        self.AddObserver("KeyPressEvent", self.KeyPressEvent)
+        self.AddObserver("CharEvent", self.DummyFunc)
+
+    def DummyFunc(self, obj, event):
+        return
+
+
+    def KeyPressEvent(self, obj, event):
+        keycode = self.GetInteractor().GetKeySym()
+
+
+        if keycode == 'w':
+            self.CamTop()
+
+        elif keycode == 'a':
+            self.CamLeft()
+
+        elif keycode == 's':
+            self.CamBottom()
+
+        elif keycode == 'd':
+            self.CamRight()
+
+
+        elif keycode == 'space':
+            self.CamForward()
+
+        elif keycode == 'c':
+            self.CamBackward()
+
+
+
+
+    def MouseMoveEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnMouseMove()
+
+    def RightButtonPressEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnRightButtonDown()
+
+    def RightButtonReleaseEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnRightButtonUp()
+
+    def LeftButtonPressEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnLeftButtonDown()
+
+    def LeftButtonReleaseEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnLeftButtonUp()
+
+    def MiddleButtonPressEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnMiddleButtonDown()
+
+    def MiddleButtonReleaseEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnMiddleButtonUp()
+
+    def MouseWheelForwardEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnMouseWheelForward()
+
+    def MouseWheelBackwardEvent(self, obj, event):
+        if self.m_bProopagateEvent :
+            self.OnMouseWheelBackward()
+
+
+    def Disable(self):
+        self.m_bProopagateEvent = False
+
+    def Enable(self):
+        self.m_bProopagateEvent = True
+
+
+    def CamTop(self):
+        camera = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().GetActiveCamera()
+
+        #View Up
+        camera.OrthogonalizeViewUp()
+        up = np.array(camera.GetViewUp())
+
+        self.MoveCamera(up*self.m_TransFactor)
+
+    def CamBottom(self):
+        camera = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().GetActiveCamera()
+
+        #View Up
+        camera.OrthogonalizeViewUp()
+        up = np.array(camera.GetViewUp())
+
+        self.MoveCamera(up*(-self.m_TransFactor))
+
+    def CamLeft(self):
+        camera = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().GetActiveCamera()
+
+        #View Up
+        camera.OrthogonalizeViewUp()
+        up = np.array(camera.GetViewUp())
+        viewDir = np.array(camera.GetDirectionOfProjection())
+
+        left = np.cross(up, viewDir)
+        self.MoveCamera(left*self.m_TransFactor)
+
+    def CamRight(self):
+        camera = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().GetActiveCamera()
+
+        #View Up
+        camera.OrthogonalizeViewUp()
+        up = np.array(camera.GetViewUp())
+        viewDir = np.array(camera.GetDirectionOfProjection())
+
+        left = np.cross(up, viewDir)
+        self.MoveCamera(left*(-self.m_TransFactor))
+
+    def CamForward(self):
+        camera = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().GetActiveCamera()
+        viewDir = np.array(camera.GetDirectionOfProjection())
+        self.MoveCamera(viewDir * self.m_TransFactor)
+
+
+    def CamBackward(self):
+        camera = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().GetActiveCamera()
+        viewDir = np.array(camera.GetDirectionOfProjection())
+        self.MoveCamera(viewDir * (-self.m_TransFactor))
+
+    def MoveCamera(self, vector):
+        camera = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().GetActiveCamera()
+
+        #Get Camera Position
+        position = np.array(camera.GetPosition())
+        focal = np.array(camera.GetFocalPoint())
+
+        updatePosition = position + vector
+        updateFocal = focal + vector
+
+        camera.SetPosition(updatePosition)
+        camera.SetFocalPoint(updateFocal)
+
+        self.Mgr.Redraw()
--- Manager/E_Manager.py
+++ Manager/E_Manager.py
@@ -1,6 +1,8 @@
 import vtk
 from vtk.util.numpy_support import vtk_to_numpy
 
+from Manager.E_Interactor import *
+
 class E_Manager:
     def __init__(self, window):
         self.m_mainFrm = window
@@ -8,32 +10,53 @@ class E_Manager:
         #Two Renderers
         self.m_renderer = [0, 0]
 
+        #Camera Actor
+        self.m_cameraActor = vtk.vtkActor()
+
 
 
 
     def Initialize(self):
         for i in range(2):
-            interactor = vtk.vtkInteractorStyleSwitch()
-
             self.m_renderer[i] = vtk.vtkRenderer()
             self.m_renderer[i].SetBackground(0.0, 0.0, 0.0)
             self.m_mainFrm.m_vtkWidget[i].GetRenderWindow().AddRenderer(self.m_renderer[i])
-            self.m_mainFrm.m_vtkWidget[i].GetRenderWindow().Render()
+            # self.m_mainFrm.m_vtkWidget[i].GetRenderWindow().Render()
+
+            interactor = E_InteractorStyle(self)
             self.m_mainFrm.m_vtkWidget[i].GetRenderWindow().GetInteractor().SetInteractorStyle(interactor)
+            self.m_renderer[i].GetActiveCamera().ParallelProjectionOff()
+
+        self.m_renderer[1].GetActiveCamera().SetPosition(3, 10, 2)
+
+        self.m_mainFrm.m_vtkWidget[i].GetRenderWindow().GetInteractor().GetInteractorStyle().Disable()
+
 
 
         self.InitDefaultObjects()
 
     def InitDefaultObjects(self):
+
+        #Texture Image
+        imgReader = vtk.vtkPNGReader()
+        imgReader.SetFileName("/Users/sim-eungjun/documents/projects/EJ_DQNTENSORFLOW/image/four.png")
+
+        texture = vtk.vtkTexture()
+        texture.SetInputConnection(imgReader.GetOutputPort())
+
         planeSource = vtk.vtkPlaneSource()
         planeSource.SetCenter(1, 0, 0)
         planeSource.SetNormal(0, 0, 1)
 
+        texturePlaneSource = vtk.vtkTextureMapToPlane()
+        texturePlaneSource.SetInputConnection(planeSource.GetOutputPort())
+
         planeMapper = vtk.vtkPolyDataMapper()
-        planeMapper.SetInputConnection(planeSource.GetOutputPort())
+        planeMapper.SetInputConnection(texturePlaneSource.GetOutputPort())
 
         planeActor = vtk.vtkActor()
         planeActor.SetMapper(planeMapper)
+        planeActor.SetTexture(texture)
 
         self.m_renderer[0].AddActor(planeActor)
         self.m_renderer[0].ResetCamera()
@@ -41,13 +64,35 @@ class E_Manager:
 
         planeActor2 = vtk.vtkActor()
         planeActor2.SetMapper(planeMapper)
+        planeActor2.SetTexture(texture)
 
         self.m_renderer[1].AddActor(planeActor2)
+
+
+        #Add Camera Actor
+        cameraSource = vtk.vtkConeSource()
+        cameraMapper = vtk.vtkPolyDataMapper()
+        cameraMapper.SetInputConnection(cameraSource.GetOutputPort())
+        self.m_cameraActor.SetMapper(cameraMapper)
+        camTrans = self.m_renderer[0].GetActiveCamera().GetViewTransformObject()
+        self.m_cameraActor.SetUserTransform(camTrans)
+
+        self.m_renderer[1].AddActor(self.m_cameraActor)
         self.m_renderer[1].ResetCamera()
+
 
         self.Redraw()
 
+    def GetCamera(self, idx):
+        return self.m_renderer[idx].GetActiveCamera()
+
+
 
     def Redraw(self):
+        self.m_cameraActor.SetPosition( self.m_renderer[0].GetActiveCamera().GetPosition() )
+
+        self.m_renderer[1].ResetCamera()
+
         for i in range(2):
-            self.m_mainFrm.m_vtkWidget[i].GetRenderWindow().Render()
+            self.m_renderer[i].GetRenderWindow().Render()
+            self.m_renderer[i].ResetCameraClippingRange ()
