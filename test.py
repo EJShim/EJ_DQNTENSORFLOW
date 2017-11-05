10ff7c9c9e89cc767bb847cbca7a314cc871a93a
ejshim <soulrommel@gmail.com>
2017-11-05 12:49:46 +0900

    update

--- test.py
+++ test.py
@@ -1,72 +1,14 @@
-
-from E_PolicyGradientBrain import *
-
-# Main FUnction
-env = gym.make('CartPole-v0')
-brain = E_PolicyGradientBrain(4, 2)
-
-num_epiosdes = 500
-max_step = 999
+import tensorflow as tf
 
 
-#Run Training
-for i in range(num_epiosdes):
-
-    state = env.reset()
-
-    brain.Reset()
-    transitions = []
-    epsilon = 1.0
-
-    done = False
-
-    rewardsAll = 0.0
-
-    for j in range(max_step):
-
-        action = brain.Forward(state, epsilon)
-        state1, reward, done, info = env.step(action)
-        transitions.append((state, action, reward))
-
-        rewardsAll += reward
-
-        if done : break
+DIM_STATE = 8
+DIM_ACTIONS = 4
 
 
-        state = state1
-
-    # #Backward
-    brain.Backward(transitions)
-
-
-    print(rewardsAll)
-
-
-for i in range(num_epiosdes):
-
-    state = env.reset()
-    brain.Reset()
-    # transitions = []
-
-    done = False
-
-    rewardsAll = 0.0
-
-    for j in range(max_step):
-        action = brain.Forward(state)
-        state1, reward, done, info = env.step(action)
-        env.render()
-        # transitions.append((state, action, reward))
-
-        rewardsAll += reward
-
-        if done : break
-
-
-        state = state1
-
-    # # #Backward
-    # brain.Backward(transitions)
-
-
-    print(rewardsAll)
+l_input = tf.placeholder(shape=[None,DIM_STATE], dtype=tf.float32)
+l_hidden1 = tf.layers.dense(inputs=l_input, units=64, activation=tf.nn.relu, name='hidden1')
+print(l_hidden1.shape)
+l_hidden2 = tf.layers.dense(inputs=l_hidden1, units=64, activation=tf.nn.relu, name='hidden2')
+print(l_hidden2.shape)
+l_out = tf.layers.dense(inputs=l_hidden2, units=DIM_ACTIONS, name='out')
+print(l_out.shape)
